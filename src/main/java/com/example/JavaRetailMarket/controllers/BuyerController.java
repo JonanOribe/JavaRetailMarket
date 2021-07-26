2030eb95ce55da5fcaa1680f888f2d25798be775
jonanOribe <jonan.oribe@gmail.com>
2021-07-26 19:48:52 +0200

    Update

--- src/main/java/com/example/JavaRetailMarket/controllers/BuyerController.java
+++ src/main/java/com/example/JavaRetailMarket/controllers/BuyerController.java
@@ -1,4 +1,7 @@
 package com.example.JavaRetailMarket.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BuyerController {
 }
--- src/main/java/com/example/JavaRetailMarket/services/AuthorService.java
+++ src/main/java/com/example/JavaRetailMarket/services/AuthorService.java
@@ -1,4 +1,7 @@
 package com.example.JavaRetailMarket.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthorService {
 }
