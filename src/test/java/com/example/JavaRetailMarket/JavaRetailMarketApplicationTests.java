34dd39dde3a4774fa56e302589febb42adc88f07
jonanOribe <jonan.oribe@gmail.com>
2021-07-26 16:36:29 +0200

    Basics

---
+++ src/test/java/com/example/JavaRetailMarket/JavaRetailMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaRetailMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRetailMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
